720e76bb90e0476b72bb26a727db5503f127e622
SimonSenger <simo671b@easv365.dk>
2021-10-12 17:42:33 +0200

    Added editorconfig and fixed some suggestions

--- Extensions/Conversion/Weight/WeightUnits.Kilograms.cs
+++ Extensions/Conversion/Weight/WeightUnits.Kilograms.cs
@@ -4,8 +4,8 @@ namespace Extensions.Conversion.Weight.Kilograms
 {
     public static partial class WeightUnits
     {
-        private static double _poundsRatio = 2.20462262;
-        private static double _stoneRatio = 6.35029318;
+        private static readonly double _poundsRatio = 2.20462262;
+        private static readonly double _stoneRatio = 6.35029318;
 
         public static double KilogramsToPounds(this double value) => Math.Round(value * _poundsRatio, 2);
 
