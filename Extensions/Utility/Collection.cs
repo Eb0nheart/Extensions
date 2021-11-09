fff0d23185f5d46f5c3be7ab43e1cdd17656a9ec
Eb0nheart <76154207+Eb0nheart@users.noreply.github.com>
2021-11-09 19:44:35 +0100

    Implemented extensions method but still left the tests failing

---
+++ Extensions/Utility/Collection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Extensions.Utility;
+
+public static class CollectionUtility
+{
+    public static void RemoveById<T>(this Collection<T> collection, int id) where T : class, new()
+    {
+
+    }
+
+    public static void RemoveById<T>(this Collection<T> collection, Guid id) where T : class, new()
+    {
+
+    }
+}
--- ExtensionsTests/UtilityTests/CollectionTests.cs
+++ ExtensionsTests/UtilityTests/CollectionTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using Extensions.Utility;
+using System.Collections.ObjectModel;
 
 namespace ExtensionsTests.UtilityTests;
 
