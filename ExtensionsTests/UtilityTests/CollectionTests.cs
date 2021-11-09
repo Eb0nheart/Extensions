a5ac2d293876b9fd7fa71f8c0a7fe4c0d3a658f7
Eb0nheart <76154207+Eb0nheart@users.noreply.github.com>
2021-11-09 19:39:36 +0100

    added first two failing tests

---
+++ ExtensionsTests/UtilityTests/CollectionTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ExtensionsTests.UtilityTests;
+
+public class CollectionTests
+{
+    [Test]
+    public void RemoveById_WorksWithIntId()
+    {
+        var objectToDelete = new TestClassWithId { IntId = 1 };
+        var testCollection = new Collection<TestClassWithId> { objectToDelete, new() { IntId = 2 }, new() { IntId = 3 } };
+
+        testCollection.RemoveById(objectToDelete.IntId);
+        var objectWhichWasSupposedToBeDeleted = testCollection.FirstOrDefault(obj => obj.IntId == objectToDelete.IntId);
+
+        Assert.IsNull(objectWhichWasSupposedToBeDeleted, "Object should have been deleted, and FirstOrDefault should therefore return null!");
+    }
+
+    [Test]
+    public void RemoveById_WorksWithGuidId()
+    {
+        var objectToDelete = new TestClassWithId { GuidId = Guid.NewGuid() };
+        var testCollection = new Collection<TestClassWithId> { objectToDelete, new() { GuidId = Guid.NewGuid() }, new() { GuidId = Guid.NewGuid() } };
+
+        testCollection.RemoveById(objectToDelete.GuidId);
+        var objectWhichWasSupposedToBeDeleted = testCollection.FirstOrDefault(obj => obj.GuidId == objectToDelete.GuidId);
+
+        Assert.IsNull(objectWhichWasSupposedToBeDeleted, "Object should have been deleted, and FirstOrDefault should therefore return null!");
+    }
+}
+
+public class TestClassWithId
+{
+    public int IntId { get; set; }
+
+    public Guid GuidId { get; set; }
+}
