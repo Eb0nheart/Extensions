3ed6f579344b10477ea381e5d5abdec652e33845
Eb0nheart <76154207+Eb0nheart@users.noreply.github.com>
2021-11-11 22:53:50 +0100

    Implemented all tests for RemoveById()

--- ExtensionsTests/UtilityTests/CollectionTests.cs
+++ ExtensionsTests/UtilityTests/CollectionTests.cs
@@ -8,31 +8,73 @@ public class CollectionTests
     [Test]
     public void RemoveById_WorksWithIntId()
     {
-        var objectToDelete = new TestClassWithId { IntId = 1 };
-        var testCollection = new Collection<TestClassWithId> { objectToDelete, new() { IntId = 2 }, new() { IntId = 3 } };
+        var objectToDelete = new TestClassWithIntId { Id = 1 };
+        var testCollection = new Collection<TestClassWithIntId> { objectToDelete, new() { Id = 2 }, new() { Id = 3 } };
 
-        testCollection.RemoveById(objectToDelete.IntId);
-        var objectWhichWasSupposedToBeDeleted = testCollection.FirstOrDefault(obj => obj.IntId == objectToDelete.IntId);
+        testCollection.RemoveById(objectToDelete.Id);
+        var objectWhichWasSupposedToBeDeleted = testCollection.FirstOrDefault(obj => obj.Id == objectToDelete.Id);
 
-        Assert.IsNull(objectWhichWasSupposedToBeDeleted, "Object should have been deleted, and FirstOrDefault should therefore return null!");
+        Assert.IsNull(objectWhichWasSupposedToBeDeleted, "Item should have been deleted, and FirstOrDefault should therefore return null!");
     }
 
     [Test]
     public void RemoveById_WorksWithGuidId()
     {
-        var objectToDelete = new TestClassWithId { GuidId = Guid.NewGuid() };
-        var testCollection = new Collection<TestClassWithId> { objectToDelete, new() { GuidId = Guid.NewGuid() }, new() { GuidId = Guid.NewGuid() } };
+        var objectToDelete = new TestClassWithGuidId { Id = Guid.NewGuid() };
+        var testCollection = new Collection<TestClassWithGuidId> { objectToDelete, new() { Id = Guid.NewGuid() }, new() { Id = Guid.NewGuid() } };
 
-        testCollection.RemoveById(objectToDelete.GuidId);
-        var objectWhichWasSupposedToBeDeleted = testCollection.FirstOrDefault(obj => obj.GuidId == objectToDelete.GuidId);
+        testCollection.RemoveById(objectToDelete.Id);
+        var objectWhichWasSupposedToBeDeleted = testCollection.FirstOrDefault(obj => obj.Id == objectToDelete.Id);
 
-        Assert.IsNull(objectWhichWasSupposedToBeDeleted, "Object should have been deleted, and FirstOrDefault should therefore return null!");
+        Assert.IsNull(objectWhichWasSupposedToBeDeleted, "Item should have been deleted, and FirstOrDefault should therefore return null!");
+    }
+
+    [Test]
+    public void RemoveById_ThrowsWithDifferentIdTypes()
+    {
+        var objectToDelete = new TestClassWithGuidId { Id = Guid.NewGuid() };
+        var testCollection = new Collection<TestClassWithIntId> { new() { Id = 1 }, new() { Id = 2 }, new() { Id = 3 } };
+
+        Assert.Throws<InvalidOperationException>(() => testCollection.RemoveById(objectToDelete.Id), "It shouldnt be possible to delete an item with Id type which is different");
+    }
+
+    [Test]
+    public void RemoveById_ThrowsWhenItemTypeDoesntHaveIdProperty()
+    {
+        var objectToDelete = new TestClassWithoutId();
+        var testCollection = new Collection<TestClassWithoutId> { new() { Age = 1 }, new() { Age = 2 }, new() { Age = 3 } };
+
+        Assert.Throws<InvalidOperationException>(() => testCollection.RemoveById(objectToDelete.Age), "It shouldnt be possible to delete an item of a type without property called Id");
+    }
+
+    [Test]
+    public void RemoveById_ThrowsWhenCalledOnEmptyCollection()
+    {
+        var testCollection = new Collection<object>();
+
+        Assert.Throws<InvalidOperationException>(() => testCollection.RemoveById(1), "It shouldnt be possible to call Remove on an empty collection");
+    }
+
+    [Test]
+    public void RemoveById_ThrowsWhenCalledOnNullCollection()
+    {
+        Collection<object> testCollection = null;
+
+        Assert.Throws<ArgumentNullException>(() => testCollection.RemoveById(1), "It shouldnt be possible to call Remove on a collection which is null");
     }
 }
 
-public class TestClassWithId
+public class TestClassWithIntId
 {
-    public int IntId { get; set; }
+    public int Id { get; set; }
+}
 
-    public Guid GuidId { get; set; }
+public class TestClassWithGuidId
+{
+    public Guid Id { get; set; }
+}
+
+public class TestClassWithoutId
+{
+    public int Age { get; set; }
 }
